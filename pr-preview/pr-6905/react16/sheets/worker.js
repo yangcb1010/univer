42ac0a9091df185600e90ea810eaadd5821fd09f
DR-Univer <DR-Univer@users.noreply.github.com>
2026-05-15 16:00:50 +0000

    Deploy preview for PR  🛫

---
+++ pr-preview/pr-6905/react16/sheets/worker.js
@@ -0,0 +1,37 @@
+import {
+  UniverSheetsFilterPlugin
+} from "../chunk-NBYWMM5G.js";
+import {
+  zh_CN_default
+} from "../chunk-QDQXZY2J.js";
+import {
+  UniverRemoteSheetsFormulaPlugin
+} from "../chunk-OXJQSR2P.js";
+import {
+  UniverFormulaEnginePlugin,
+  UniverRPCWorkerThreadPlugin,
+  UniverSheetsPlugin
+} from "../chunk-3WKLWIVD.js";
+import "../chunk-XUATXRUF.js";
+import {
+  Univer
+} from "../chunk-YOA52FQR.js";
+import "../chunk-EQ2B2W73.js";
+import "../chunk-24OICD5T.js";
+
+// src/sheets/worker.ts
+var univer = new Univer({
+  locale: "zhCN" /* ZH_CN */,
+  logLevel: 4 /* VERBOSE */,
+  locales: {
+    ["zhCN" /* ZH_CN */]: zh_CN_default
+  }
+});
+univer.registerPlugins([
+  [UniverSheetsPlugin, { onlyRegisterFormulaRelatedMutations: true }],
+  [UniverFormulaEnginePlugin],
+  [UniverRPCWorkerThreadPlugin],
+  [UniverRemoteSheetsFormulaPlugin],
+  [UniverSheetsFilterPlugin]
+]);
+self.univer = univer;
